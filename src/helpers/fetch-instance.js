83d4f6d9b4a68a7d6dcf8420a1f66084cb9c8950
afdal <afd.zik@gmail.com>
2024-09-05 00:04:48 +0700

    fix: instance

--- src/helpers/fetch-instance.js
+++ src/helpers/fetch-instance.js
@@ -13,11 +13,11 @@ const ignored_code = [401, 422];
 // helper functions
 function authHeader(url) {
   // return auth header with jwt if user is logged in and request is to the api url
-  const { user } = useAuthStore();
-  const isLoggedIn = !!user?.token;
+  const { token } = useAuthStore();
+  const isLoggedIn = !!token;
   const isApiUrl = url.startsWith(import.meta.env.VITE_API_URL);
   if (isLoggedIn && isApiUrl) {
-    return { Authorization: `Bearer ${user.token}` };
+    return { Authorization: `Bearer ${token}` };
   } else {
     return {};
   }
